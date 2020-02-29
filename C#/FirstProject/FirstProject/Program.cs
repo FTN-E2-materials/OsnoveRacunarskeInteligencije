4a616786f328ea40d67df73c9d8734957bf45d75
vlaksi <daksrapsody@gmail.com>
2020-02-29 19:41:00 +0100

    first c# project

---
+++ C#/FirstProject/FirstProject/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Ispisuje nesto na ekran i zamrzava ekran dok korisnik ne unese 
+             * nesto sto ce prekinuti rad programa.
+             * 
+             */
+            Console.WriteLine("DJESI MIMA I STA IMA");          
+            Console.ReadLine();
+        }
+    }
+}
