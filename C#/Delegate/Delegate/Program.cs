69d006621932377fbf3c9bed4eb352d12c4b8c99
vlaksi <daksrapsody@gmail.com>
2020-03-01 15:24:29 +0100

    Delegate in C#

---
+++ C#/Delegate/Delegate/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+// Delegate Declaration. 
+public delegate void HelloFunctionDelegate(string Message);
+
+
+class Program
+{
+    public static void Main()
+    {
+        // Create the instance of the delegate and pass in the function
+        // name as the parameter to the constructor. The passed in
+        // function signature must match the signature of the delegate
+        HelloFunctionDelegate del = new HelloFunctionDelegate(Hello);
+        // Invoke the delegate, which will invoke the method
+        del("Hello from Delegte");
+
+        Console.ReadLine();
+    }
+
+
+    public static void Hello(string strMessge)
+    {
+        Console.WriteLine(strMessge);
+    }
+}
