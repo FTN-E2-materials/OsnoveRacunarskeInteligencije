7be996316e75af36979de06a2a3a33d32be45b8b
Pufke <pufke1998@gmail.com>
2020-03-01 01:32:37 +0100

    Delegate primer

---
+++ C#/LambdaIzrazi/LambdaIzrazi/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace LambdaIzrazi
+{
+    class Program
+    {
+        //delegat polje, može mu se dodeliti bilo kakva funkcija sa int parametrom.
+        delegate int del(int i);
+        //delegat sa dva parametra.
+        delegate int del2(int i, int j);
+
+        static void Main(string[] args)
+        {
+
+            del myDelegate = x => x * x;
+            int rez = myDelegate(5); //rez = 25
+            del2 myDelegate2 = (x1, x2) => x1 * x2;
+            int rez2 = myDelegate2(5, 5); //rez = 25
+
+            Console.Write("Rez1: "+ rez + " Rez2: "+ rez2);
+
+        }
+    }
+}
