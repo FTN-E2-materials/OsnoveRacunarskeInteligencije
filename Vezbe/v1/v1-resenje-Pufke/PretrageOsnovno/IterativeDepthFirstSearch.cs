ec22348e3b21bad3207e8fa0c80c4120c432bb10
Pufke <pufke1998@gmail.com>
2020-03-13 22:37:48 +0100

    Vezbe 1 Iterativni prvi u dubinu implementiran

--- Vezbe/v1/v1-resenje-Pufke/PretrageOsnovno/IterativeDepthFirstSearch.cs
+++ Vezbe/v1/v1-resenje-Pufke/PretrageOsnovno/IterativeDepthFirstSearch.cs
@@ -14,6 +14,37 @@ namespace PretrageOsnovno
         // TODO 6: implementirati algoritam iterativni prvi u dubinu
         public State Search(string startNodeName, string endNodeName)
         {
+            Node startNode = Program.instance.graph[startNodeName];
+            Node endNode = Program.instance.graph[endNodeName];
+
+            for(int level = 0; level < MaxLevel; level++)
+            {
+                //Isti kod kao kod DFS-a
+                List<State> zaObradu = new List<State>();
+                zaObradu.Add(new State(startNode));
+
+                while(zaObradu.Count > 0)
+                {
+                    State naObradi = zaObradu[0];
+                    zaObradu.Remove(naObradi);
+                    //Proveravamo da li je na obradi lvl > lvl
+                    if (naObradi.Level > level)
+                        continue;
+                    //Ako jeste preskacemo ga ako nije nastavljamo dalje 
+                    if(naObradi.Node.Name == endNode.Name)
+                    {
+                        return naObradi;
+                    }
+                    else
+                    {
+                        List<State> mogucaSledecaStanja = naObradi.children();
+                        foreach (State next in mogucaSledecaStanja)
+                        {
+                            zaObradu.Insert(0, next);
+                        }
+                    }
+                }
+            }
             return null;
         }
     }
