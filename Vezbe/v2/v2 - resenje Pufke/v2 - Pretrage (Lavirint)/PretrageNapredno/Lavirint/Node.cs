b932af2b506de0883668bb055c33aaa38ba31637
Pufke <pufke1998@gmail.com>
2020-03-14 14:41:40 +0100

    Vezbe 2, implementirana metoda koja odredjuje dozvoljeno kretanje getLinkedNodes(), i implementirana metoda validCords()

--- Vezbe/v2/v2 - resenje Pufke/v2 - Pretrage (Lavirint)/PretrageNapredno/Lavirint/Node.cs
+++ Vezbe/v2/v2 - resenje Pufke/v2 - Pretrage (Lavirint)/PretrageNapredno/Lavirint/Node.cs
@@ -17,6 +17,16 @@ namespace Lavirint
         private bool validCoords(int markI, int markJ)
         {
             // TODO 2: Implementirati logiku za validna/nevalidna stanja iz zabranu prolaska kroz zidove
+            if (markI < 0 || markJ < 0 || markI > Main.lavirint.brojVrsta || markJ >= Main.lavirint.brojKolona)
+            {
+                return false;
+            }
+
+            if(Main.lavirint.polja[markI, markJ] == 1)
+            {
+                return false;
+            }
+            //Ako nije prolazak kroz zid ili ako nije vec popunjeno polje to znaci da je VALIDNO
 
             return true;
         }
@@ -25,6 +35,23 @@ namespace Lavirint
         {
             // TODO 1: Implementirati metodu tako da odredjuje dozvoljeno kretanje u lavirintu.
             List<Node> nextNodes = new List<Node>();
+
+            int[,] movements = new int[,] { { 0, 1 }, { 0, -1 }, { 1, 0 }, { -1, 0 } };
+
+            for(int i = 0; i < movements.GetLength(0); ++i)
+            {
+                int newMarkI = movements[i, 0] + this.markI;
+                int newMarkJ = movements[i, 1] + this.markJ;
+                //sa this.markI dobijamo trenutnu poziciju cvora u GRIDU po I koridinati
+                //sa this.markJ dobijamo trenutnu poziciju cvora u GRIDU po J kordinati
+
+                //Proveravamo da li je taj cvor valdan 
+                if(validCoords(newMarkI, newMarkJ))
+                {
+                    nextNodes.Add(new Node(newMarkI, newMarkJ));
+                }
+            }
+
             
             return nextNodes;
         }
