9ba455a1e24468e53a66c5225df6d18fde411b55
Pufke <pufke1998@gmail.com>
2020-03-14 16:16:28 +0100

    v2 Implementirana A* pretragu i heuristička funkcija koju će ta pretraga koristiti

--- Vezbe/v2/v2 - resenje Pufke/v2 - Pretrage (Lavirint)/PretrageNapredno/Lavirint/AStarSearch.cs
+++ Vezbe/v2/v2 - resenje Pufke/v2 - Pretrage (Lavirint)/PretrageNapredno/Lavirint/AStarSearch.cs
@@ -10,6 +10,24 @@ namespace Lavirint
         public State search(State pocetnoStanje)
         {
             // TODO 5.1: Implementirati algoritam vodjene pretrage A*
+            List<State> stanjaNaObradi = new List<State>();
+            stanjaNaObradi.Add(pocetnoStanje);
+            while(stanjaNaObradi.Count > 0)
+            {
+                State naObradi = getBest(stanjaNaObradi);
+
+                if (!naObradi.cirkularnaPutanja())
+                {
+                    Main.allSearchStates.Add(naObradi);
+                    if (naObradi.isKrajnjeStanje())
+                    {
+                        return naObradi;
+                    }
+                    List<State> mogucaSledecaStanja = naObradi.mogucaSledecaStanja();
+                    stanjaNaObradi.AddRange(mogucaSledecaStanja);
+                }
+                stanjaNaObradi.Remove(naObradi);
+            }
             return null;
         }
 
@@ -17,7 +35,25 @@ namespace Lavirint
         public double heuristicFunction(State s)
         {
             // TODO 5.2: Implementirati heuristicku funkciju (funkcija odredjuje rastojanje)
-            return 0.0;
+            return Math.Sqrt(Math.Pow(s.node.markI - Main.krajnjiNode.markI, 2)
+                + Math.Pow(s.node.markJ - Main.krajnjiNode.markJ, 2));
+        }
+
+        public State getBest(List<State> stanja)
+        {
+            State rez = null;
+            double min = Double.MinValue;
+
+            foreach(State s in stanja)
+            {
+                double h = heuristicFunction(s) + s.cost;
+                if(h < min)
+                {
+                    min = h;
+                    rez = s;
+                }
+            }
+            return rez;
         }
     }
 }
