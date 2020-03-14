c9dd9eb77ec39dcd8fd37d97806f5c4c3c4ee563
vlaksi <daksrapsody@gmail.com>
2020-03-14 19:48:33 +0100

    v2[todo5] implemented A* and heuristicFunction()

--- Vezbe/v2/v2-resenje-Vaksi/v2 - Pretrage (Lavirint)/PretrageNapredno/Lavirint/AStarSearch.cs
+++ Vezbe/v2/v2-resenje-Vaksi/v2 - Pretrage (Lavirint)/PretrageNapredno/Lavirint/AStarSearch.cs
@@ -10,6 +10,24 @@ namespace Lavirint
         public State search(State pocetnoStanje)
         {
             // TODO 5.1: Implementirati algoritam vodjene pretrage A*
+            List<State> stanjaNaObradi = new List<State>();
+            stanjaNaObradi.Add(pocetnoStanje);
+
+            while(stanjaNaObradi.Count > 0)
+            {
+                State naObradi = getBest(stanjaNaObradi);
+                if (!naObradi.cirkularnaPutanja())
+                {
+                    Main.allSearchStates.Add(naObradi);             // dodajemo polja koja su proveravana
+                    if (naObradi.isKrajnjeStanje())
+                    {
+                        return naObradi;
+                    }
+                    List<State> mogucaSledecaStanja = naObradi.mogucaSledecaStanja();
+                    stanjaNaObradi.AddRange(mogucaSledecaStanja);   // dodajemo listu elemenata u listu
+                }
+                stanjaNaObradi.Remove(naObradi);
+            }
             return null;
         }
 
@@ -17,7 +35,26 @@ namespace Lavirint
         public double heuristicFunction(State s)
         {
             // TODO 5.2: Implementirati heuristicku funkciju (funkcija odredjuje rastojanje)
-            return 0.0;
+            return Math.Sqrt(Math.Pow(s.node.markI - Main.krajnjiNode.markI, 2)
+                + Math.Pow(s.node.markJ - Main.krajnjiNode.markJ, 2));
+        }
+
+        // Funkcija koja od svih stanja vraca ono najbolje.
+        public State getBest(List<State> stanja)
+        {
+            State rez = null;
+            double min = Double.MaxValue;
+
+            foreach (State s in stanja)
+            {   // u nasem slucaju najmanja vrednost je najbolja
+                double h = heuristicFunction(s) + s.cost;
+                if (h < min)
+                {
+                    min = h;
+                    rez = s;
+                }
+            }
+            return rez;
         }
     }
 }
