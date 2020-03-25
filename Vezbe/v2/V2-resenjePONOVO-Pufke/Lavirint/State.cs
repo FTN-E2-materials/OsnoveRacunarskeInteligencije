1932e45c847fb1d174b5a0af6816c6a3b329e353
Pufke <pufke1998@gmail.com>
2020-03-26 00:21:50 +0100

    V2 TODO1 Implementirana metoda koja odredjuje dozvoljeno kretanje u lavirintu

---
+++ Vezbe/v2/V2-resenjePONOVO-Pufke/Lavirint/State.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lavirint
+{
+    public class State //Klasa State modeluje jedno stanje u lavirintu i sadrzi sve informacije koje su nam potrebne
+    {
+        public State parent;     // Ko je roditeljski state
+        public Node node;        // O kom cvoru je rec
+        public double cost;      // Cena
+        public int level;        // i nivo
+
+        public State sledeceStanje(Node node)
+        {
+            State rez = new State();
+            rez.node = node;
+            rez.parent = this;
+            rez.cost = this.cost + 1;
+            rez.level = this.level + 1;
+            return rez;
+        }
+
+
+        public List<State> mogucaSledecaStanja()
+        {
+            List<State> rez = new List<State>();
+            foreach (Node nextNode in this.node.getLinkedNodes())
+            {
+                rez.Add(this.sledeceStanje(nextNode));
+            }
+            return rez;
+        }
+
+        public bool isKrajnjeStanje()
+        {
+            return Main.krajnjiNode.Equals(this.node);
+        }
+
+        public List<State> path()
+        {
+            List<State> putanja = new List<State>();
+            State tt = this;
+            while (tt != null)
+            {
+                putanja.Insert(0, tt);
+                tt = tt.parent;
+            }
+            return putanja;
+        }
+
+        public bool cirkularnaPutanja()
+        {
+            // TODO 3: proveriti da li trenutno stanje odgovara poziciji koja je vec vidjena u grani pretrazivanja
+            
+            return false;
+        }
+    }
+}
