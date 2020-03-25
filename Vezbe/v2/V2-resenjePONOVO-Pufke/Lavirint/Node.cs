03023a1e9cf248c51cc18278af279f81a6ab7e8d
Pufke <pufke1998@gmail.com>
2020-03-25 19:13:02 +0100

    V2 , ponovno resavanje kompletnih vezbi, inicijalni komit

---
+++ Vezbe/v2/V2-resenjePONOVO-Pufke/Lavirint/Node.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lavirint
+{
+    public class Node
+    {
+        public int markI, markJ;
+
+        public Node(int markI, int markJ)
+        {
+            this.markI = markI;
+            this.markJ = markJ;
+        }
+
+        private bool validCoords(int markI, int markJ)
+        {
+            // TODO 2: Implementirati logiku za validna/nevalidna stanja iz zabranu prolaska kroz zidove
+
+            return true;
+        }
+
+        public List<Node> getLinkedNodes()
+        {
+            // TODO 1: Implementirati metodu tako da odredjuje dozvoljeno kretanje u lavirintu.
+            List<Node> nextNodes = new List<Node>();
+            
+            return nextNodes;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            Node node = (Node)obj;
+            return this.markI == node.markI && this.markJ == node.markJ;
+        }
+
+        public override int GetHashCode()
+        {
+            return 100 * this.markI + this.markJ;
+        }
+    }
+}
