81003894e15b12d31c221b5720f94d4af22ae1d6
Pufke <pufke1998@gmail.com>
2020-03-28 01:51:46 +0100

    V2 TODO 5,1 5,2 , implementiran algoritam za A* pretragu i heeuristika, istestirano radi

--- Vezbe/v2/V2-resenjePONOVO-Pufke/Lavirint/AStarSearch.cs
+++ Vezbe/v2/V2-resenjePONOVO-Pufke/Lavirint/AStarSearch.cs
@@ -7,17 +7,62 @@ namespace Lavirint
 {
     class AStarSearch
     {
+        //Razlika izmedju slepih pretraga i A* je sto smo kod njih uzimali prvi element a sada cemo najbolji getBest()
         public State search(State pocetnoStanje)
         {
             // TODO 5.1: Implementirati algoritam vodjene pretrage A*
+            List<State> stanjaNaObradi = new List<State>();
+            stanjaNaObradi.Add(pocetnoStanje);
+            while (stanjaNaObradi.Count > 0)
+            {
+                State naObradi = getBest(stanjaNaObradi);
+
+                if (!naObradi.cirkularnaPutanja())
+                {
+                    Main.allSearchStates.Add(naObradi);
+                    if (naObradi.isKrajnjeStanje())
+                    {
+                        return naObradi;
+                    }
+                    List<State> mogucaSledecaStanja = naObradi.mogucaSledecaStanja();
+                    stanjaNaObradi.AddRange(mogucaSledecaStanja);
+                }
+                stanjaNaObradi.Remove(naObradi);
+            }
+
             return null;
         }
 
-        
+        //Procena koliko je neki cvor dobar
+        //Euklidsko rastojanje od krajnjeg cvora i tog trenutnog cvora
+        //Procena koliko je neki cvor dobar
         public double heuristicFunction(State s)
         {
             // TODO 5.2: Implementirati heuristicku funkciju (funkcija odredjuje rastojanje)
-            return 0.0;
+            return Math.Sqrt(Math.Pow(s.node.markI - Main.krajnjiNode.markI, 2)
+               + Math.Pow(s.node.markJ - Main.krajnjiNode.markJ, 2));
         }
+
+        //sad trazimo najbolji po heuristika + cena
+        //Trazenje moinimuma, trazimo element gde je heuristika i
+        //cena najmanja i njega vratimo kao element
+        public State getBest(List<State> stanja)
+        {
+            State rez = null;
+            double min = Double.MaxValue;
+
+            foreach(State s in stanja)
+            {
+                double h = heuristicFunction(s) + s.cost;
+                if(h< min)
+                {
+                    min = h;
+                    rez = s;
+                }
+
+            }
+            return rez;
+        }
+       
     }
 }
